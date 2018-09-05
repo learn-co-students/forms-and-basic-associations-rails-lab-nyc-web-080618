6742aea9d45db276c33c9f182383d8d16f09f16b
Yan Wang <Yan@Flatirons-Air-8.fios-router.home>
2018-09-04 22:26:05 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,43 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def note_contents=(note_contents)
+    note_contents.each do |content|
+      if content != ""
+        note = Note.find_or_create_by(content: content)
+        self.notes << note
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map do |note|
+      note.content
+    end
+  end
+
 end
